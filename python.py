d204e23350f50255dd605684f138d0de4c143f27
Mesaye Addisu <67935330+mesaye85@users.noreply.github.com>
2023-03-14 21:50:51 -0400

    Update python.py

--- python.py
+++ python.py
@@ -3,10 +3,10 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
 # Retrieve data from API
-url = "https://api.worldbank.org/v2/countries/all/indicators/NY.GDP.MKTP.CD?format=json&date=2010:2021"
+url = "https://api.worldbank.org/v2/countries/all/indicators/NY.GDP.MKTP.CD?format=json&date=2010:2021&per_page=20000"
 response = requests.get(url)
 data = response.json()
 
@@ -14,11 +14,20 @@ data = response.json()
 df = pd.DataFrame(data[1])
 df.dropna(inplace=True)
 
-# Train the model
-X = df.drop("value", axis=1)
+# Extract year and country code
+df['year'] = df['date'].astype(int)
+df['country_code'] = df['countryiso3code']
+
+# Encode country codes as numerical values
+le = LabelEncoder()
+df['country_code'] = le.fit_transform(df['country_code'])
+
+# Prepare data for modeling
+X = df[['year', 'country_code']]
 y = df["value"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+# Train the model
 model = RandomForestRegressor()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
